4531d7e72ce3d9bcc85ddecf5918a7161576f9fe
Josh Fletcher <slwd89@gmail.com>
2019-04-21 22:04:25 -0700

    Get start and end time from the user.

--- smartapps/bigtuna00/porch-lights-with-motion.src/porch-lights-with-motion.groovy
+++ smartapps/bigtuna00/porch-lights-with-motion.src/porch-lights-with-motion.groovy
@@ -14,88 +14,91 @@
  *
  */
 definition(
- name: "Porch Lights With Motion",
- namespace: "Bigtuna00",
- author: "Josh Fletcher",
- description: "Prevent infinite lighting loops when using Ring Doorbell motion sensor to turn lights on.",
- category: "Convenience",
- iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
- iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
- iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+  name: "Porch Lights With Motion",
+  namespace: "Bigtuna00",
+  author: "Josh Fletcher",
+  description: "Prevent infinite lighting loops when using Ring Doorbell motion sensor to turn lights on.",
+  category: "Convenience",
+  iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+  iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+  iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
 
 
 preferences {
- section("When there’s movement...") {
-  input "motion1", "capability.motionSensor", title: "Where?", required: true, multiple: true
- }
- section("Turn on a light...") {
-  input "switch1", "capability.switch", required: true, multiple: true
- }
+  section("When there’s movement...") {
+    input "motion1", "capability.motionSensor", title: "Where?", required: true, multiple: false
+  }
+  section("Turn on a light...") {
+    input "switch1", "capability.switch", required: true, multiple: false
+  }
+  section("Turn on between what times?") {
+    input "fromTime", "time", title: "From", required: true
+    input "toTime", "time", title: "To", required: true
+  }
 }
 
 def installed() {
- initialize()
+  initialize()
 }
 
 def updated() {
- unsubscribe()
- initialize()
+  unsubscribe()
+  initialize()
 }
 
 def initialize() {
- //subscribe(location, "sunset", sunsetHandler)
- //subscribe(location, "sunrise", sunriseHandler)
- subscribe(switch1, "switch.off", switchHandler)
- schedule("0 0 12 * * ?", sunriseSunsetHandler)
- if (issunset()) {
-  subscribe(motion1, "motion.active", motionActiveHandler)
- }
- sunriseSunsetHandler()
+  subscribe(switch1, "switch.off", switchHandler)
+  schedule("0 0 12 * * ?", sunriseSunsetHandler)
+  if (isactive()) {
+    subscribe(motion1, "motion.active", motionActiveHandler)
+  }
+  sunriseSunsetHandler()
 }
 
 def motionActiveHandler(evt) {
- log.debug "Motion detected"
- switch1.on()
- runIn(60 * 5, OffTimeHandler)
+  log.debug "Motion detected"
+  switch1.on()
+  runIn(60 * 5, OffTimeHandler)
 }
 
 def switchHandler(evt) {
- log.debug "Switch turned off"
- runIn(15, reupHandler)
+  log.debug "Switch turned off"
+  runIn(15, reupHandler)
 }
 
 def OffTimeHandler() {
- log.debug "OffTimeHandler"
- unsubscribe(motion1)
- switch1.off()
- runIn(15, reupHandler)
+  log.debug "OffTimeHandler"
+  unsubscribe(motion1)
+  switch1.off()
+  runIn(15, reupHandler)
 }
 
 def reupHandler() {
- log.debug "reupHandler"
- if (issunset()) {
-  subscribe(motion1, "motion.active", motionActiveHandler)
- }
+  log.debug "reupHandler"
+  if (isactive()) {
+    subscribe(motion1, "motion.active", motionActiveHandler)
+  }
 }
 
+def isactive() {
+  def between = timeOfDayIsBetween(fromTime, toTime, new Date(), location.timeZone)
+  return between
+  log.debug "Active?:${between}"
+}
+
+// I beleive these are no longer really needed since I'm using start and end times
+// from the user.
 def sunsetHandler() {
- subscribe(motion1, "motion.active", motionActiveHandler)
+  subscribe(motion1, "motion.active", motionActiveHandler)
 }
 
 def sunriseHandler() {
- unsubscribe(motion1)
- switch1.off()
+  unsubscribe(motion1)
+  switch1.off()
 }
-def issunset() {
- def WRTimes = getSunriseAndSunset(zipCode: "95046")
- def between = timeOfDayIsBetween(WRTimes.sunset, WRTimes.sunrise, new Date(), location.timeZone)
- log.debug "Sunset?:${between}"
- return between
-}
-
 def sunriseSunsetHandler() {
- log.debug "Setting Sunrise Sunset times"
- def WRTimes = getSunriseAndSunset(zipCode: "95046")
- schedule(WRTimes.sunrise, sunriseHandler)
- schedule(WRTimes.sunset, sunsetHandler)
+  log.debug "Setting Sunrise Sunset times"
+  def WRTimes = getSunriseAndSunset(zipCode: "95046")
+  schedule(WRTimes.sunrise, sunriseHandler)
+  schedule(WRTimes.sunset, sunsetHandler)
 }
